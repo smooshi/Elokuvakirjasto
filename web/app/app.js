979e545e6711b231c2c6f4ae03f1ba82a940d98a
smooshi <timereh@gmail.com>
2015-12-07 19:13:05 +0200

    vaksdjalkjd

--- web/app/app.js
+++ web/app/app.js
@@ -1,1 +1,28 @@
 // Toteuta moduulisi tänne
+
+var MovieApp = angular.module('MovieApp', ['firebase']);
+
+MovieApp.controller('MovieController', function($scope, FirebaseService) {
+	console.log("Controller called");
+	this.addMovie = function() {
+		
+	}
+	
+	this.listMovies = function() {
+		
+	}
+	
+});
+
+MovieApp.service('FirebaseService', function($firebaseArray){
+	var firebaseRef = new Firebase('fiery-heat-9158.firebaseIO.com/movies');
+	var movies = $firebaseArray(firebaseRef);
+
+	this.getMovies = function(){
+	  return movies;
+	}
+	
+	this.addMovies = function(movie){
+	  movies.$add(data);
+	}
+});
